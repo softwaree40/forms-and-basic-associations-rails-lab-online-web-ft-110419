afa8a3271626bc7c2a4f2f5a8155f703ef0b5e56
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-07 21:13:22 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,17 @@
 class Song < ActiveRecord::Base
   # add associations here
+    belongs_to :artist 
+    belongs_to :genre
+    has_many :notes
+    accepts_nested_attributes_for :notes
+    def artist_name=(name)
+        self.artist = Artist.find_or_create(name: name)
+    end
+    def artist_name
+       if self.artist 
+         self.artist.name
+       else 
+         nil 
+       end
+    end
 end
