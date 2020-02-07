ab5fc8224c73e1c47d31c9b05557fccdb2ed35eb
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-07 22:05:42 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,6 +9,8 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+    @song.note.build(content: "Hello World")
+    @song.note.build
   end
 
   def create
@@ -47,7 +49,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name,:genre_id)
+    params.require(:song).permit(:title, :artist_name,:genre_id,notes_attributes: [:content])
   end
 end
 
